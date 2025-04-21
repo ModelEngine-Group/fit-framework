3b3b86e8094609fd1ae6fb7e2914bd6c50ff8297
杭潇 <1576730710@qq.com>
2025-04-21 09:53:38 +0800

    [fel] 升级插件的结构

--- examples/fel-example/06-agent/src/main/java/modelengine/example/ai/chat/agent/tool/WeatherServiceImpl.java
+++ examples/fel-example/06-agent/src/main/java/modelengine/example/ai/chat/agent/tool/WeatherServiceImpl.java
@@ -7,6 +7,7 @@
 package modelengine.example.ai.chat.agent.tool;
 
 import modelengine.fel.tool.annotation.Attribute;
+import modelengine.fel.tool.annotation.Group;
 import modelengine.fel.tool.annotation.ToolMethod;
 import modelengine.fitframework.annotation.Component;
 import modelengine.fitframework.annotation.Fitable;
@@ -15,10 +16,12 @@ import modelengine.fitframework.annotation.Property;
 /**
  * 表示 {@link WeatherService} 的默认实现。
  *
+ * @author 杭潇
  * @author 易文渊
  * @since 2024-09-02
  */
 @Component
+@Group(name = "default_weather_service")
 public class WeatherServiceImpl implements WeatherService {
     @Override
     @Fitable("default")
@@ -28,15 +31,16 @@ public class WeatherServiceImpl implements WeatherService {
                     @Attribute(key = "attribute", value = "nothing"),
                     @Attribute(key = "attribute", value = "nothing two")
             })
-    public String getCurrentTemperature(@Property(description = "城市名称", required = true) String location,
-            @Property(description = "使用的温度单位，可选：Celsius，Fahrenheit", defaultValue = "Celsius") String unit) {
+    @Property(description = "当前温度的结果")
+    public String getCurrentTemperature(String location, String unit) {
         return "26";
     }
 
     @Override
     @Fitable("default")
     @ToolMethod(namespace = "example", name = "get_rain_probability", description = "获取指定城市下雨的概率")
-    public String getRainProbability(@Property(description = "城市名称", required = true) String location) {
+    @Property(description = "下雨的概率")
+    public String getRainProbability(String location) {
         return "0.06";
     }
 }
