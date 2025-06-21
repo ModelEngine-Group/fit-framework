bacf73eed5465a8a00fa029aeac97a4228a51341
xiaohaoFly <932379959@qq.com>
2025-06-21 20:25:50 +0800

    [fit] 增加nacos的使用example以及一些类的注释

--- framework/fit/java/fit-builtin/plugins/fit-service-coordination-nacos/src/main/java/modelengine/fit/service/server/Notify.java
+++ framework/fit/java/fit-builtin/plugins/fit-service-coordination-nacos/src/main/java/modelengine/fit/service/server/Notify.java
@@ -6,25 +6,24 @@
 
 package modelengine.fit.service.server;
 
-/**
- * @author pc
- * @description
- * @created 2025/6/20
- */
-
 import modelengine.fit.service.entity.FitableAddressInstance;
 import modelengine.fitframework.annotation.Genericable;
 
 import java.util.List;
 
 /**
- * 表示运行时 {@code 'worker.'} 前缀的配置项。
+ * 表示通知服务，用于更新Fitables实例信息。
  *
  * @author 董智豪
  * @since 2025-06-20
  */
 public interface Notify {
 
+    /**
+     * 通知更新Fitables 实例。
+     *
+     * @param fitableInstances 表示指定服务实现的所有实例信息的 {@link List}{@code <}{@link FitableAddressInstance}{@code >}。
+     */
     @Genericable(id = "b69df5e8cbcd4166aa5029602e7a58cf")
     void notifyFitables(List<FitableAddressInstance> fitableInstances);
 }
